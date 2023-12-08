7522ea039e311bca174e5ed4ed952fe84fa48dbd
Radek Novak <radek.novak.cze@gmail.com>
2023-12-08 08:31:05 +0100

    template improvements

--- advent-2023/00.ts
+++ advent-2023/00.ts
@@ -1,11 +1,23 @@
 import {
+  assertArrayIncludes,
   assertObjectMatch,
   assertEquals,
 } from "https://deno.land/std@0.208.0/assert/mod.ts";
 
+const example = `
+`;
+
 async function main() {
   const file = await Deno.readTextFile("input.txt");
-  const lines = file.trim().split("\n");
+
+  // const parsed = parseFile(file);
+  const parsed = parseFile(example);
+}
+
+function parseFile(input: string) {
+  const lines = input.trim().split("\n");
+
+  return lines;
 }
 
 main();
@@ -13,9 +25,12 @@ main();
 //
 // Tests
 //
+Deno.test("simpl", () => {
+  // assertEquals(  );
+});
 Deno.test("obj", () => {
   // assertObjectMatch(  );
 });
-Deno.test("simpl", () => {
-  // assertEquals(  );
+Deno.test("arr", () => {
+  // assertArrayIncludes
 });
