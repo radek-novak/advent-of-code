aa9f175305a530c2776a79000bb188cb216540f0
Radek Novak <radek.novak.cze@gmail.com>
2023-12-15 22:32:59 +0100

    15b

--- advent-2023/15/15.ts
+++ advent-2023/15/15.ts
@@ -6,16 +6,101 @@ import {
 
 const example = `rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7`;
 
+type Lens = {
+  label: string;
+  focalLength: number;
+};
+
+type LensOp =
+  | {
+      op: string;
+      focalLength: number;
+      opHash: number;
+      arg: string;
+    }
+  | {
+      op: string;
+      opHash: number;
+      arg: string;
+      focalLength?: undefined;
+    };
+
+class Box {
+  lenses: Lens[] = [];
+  labelToIdx: Record<string, number> = {};
+  n: number;
+
+  constructor(n: number) {
+    this.n = n;
+  }
+
+  newLens(lensOp: LensOp) {
+    if (lensOp.arg === "=") {
+      this.addLens(lensOp.op, lensOp.focalLength!);
+    } else if (lensOp.arg === "-") {
+      this.removeLens(lensOp.op);
+    } else {
+      throw new Error(`Invalid lensOp: ${lensOp}`);
+    }
+  }
+
+  private addLens(label: string, focalLength: number) {
+    const idx = this.labelToIdx[label];
+    if (idx !== undefined) {
+      this.lenses[idx].focalLength = focalLength;
+      return;
+    }
+    this.lenses.push({ label, focalLength });
+    this.labelToIdx[label] = this.lenses.length - 1;
+  }
+
+  private removeLens(label: string) {
+    const idx = this.labelToIdx[label];
+    if (idx === undefined) {
+      return;
+    }
+    this.lenses.splice(idx, 1);
+
+    // push all the following indices down
+    for (let i = idx; i < this.lenses.length; i++) {
+      this.labelToIdx[this.lenses[i].label] = i;
+    }
+    delete this.labelToIdx[label];
+  }
+
+  getFocalLength() {
+    return this.lenses.reduce((acc, cur, i) => {
+      // console.log(cur.focalLength, i, this.n);
+      return acc + (this.n + 1) * (i + 1) * cur.focalLength;
+    }, 0);
+  }
+}
+
 async function main() {
   const file = await Deno.readTextFile("input.txt");
 
   const parsed = parseFile(file);
   // const parsed = parseFile(example);
 
-  const hashes = parsed.map(runningHash);
+  // Part 1
+  // const hashes = parsed.map(runningHash);
+  // const sum = hashes.reduce((a, b) => a + b, 0);
+  // console.log(sum);
 
-  const sum = hashes.reduce((a, b) => a + b, 0);
-  console.log(sum);
+  // Part 2
+  const boxes = Array.from({ length: 256 })
+    .fill(0)
+    .map((_, i) => new Box(i));
+  const lensOps = parseOperations(parsed);
+
+  for (const lensOp of lensOps) {
+    // console.log(lensOp.op, lensOp.arg);
+    const box = boxes[lensOp.opHash];
+
+    box.newLens(lensOp);
+  }
+
+  console.log(boxes.reduce((acc, cur) => acc + cur.getFocalLength(), 0));
 }
 
 function parseFile(input: string) {
@@ -25,6 +110,33 @@ function parseFile(input: string) {
     .map((line) => line.trim());
 
   return lines;
+}
+
+function parseOperations(parsed: string[]) {
+  const operations = parsed.map((line) => {
+    if (line.includes("=")) {
+      const [op, focalLength] = line.split("=");
+
+      return {
+        op,
+        focalLength: Number(focalLength),
+        opHash: runningHash(op),
+        arg: "=",
+      };
+    } else if (line.includes("-")) {
+      const op = line.replace("-", "");
+
+      return {
+        op,
+        opHash: runningHash(op),
+        arg: "-",
+      };
+    } else {
+      throw new Error(`Invalid line: ${line}`);
+    }
+  });
+
+  return operations;
 }
 
 main();
